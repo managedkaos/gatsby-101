cd36463ceef4d83dbcb57c6b1f3b19d7ce508b7b
Michael <michael@managedkaos.com>
2020-02-02 12:21:23 -0800

    Add styling to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,11 +4,55 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import styled from "@emotion/styled"
+
+const AboutWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 0;
+  margin-right: auto;
+  margin-bottom: 12px;
+  &:last-child {
+    margin-bottom: 0
+  }
+`
+
+const Description = styled.div`
+  flex:1;
+  margin-left: 18px;
+  padding: 12px;
+`
+
+const EventName = styled.h3`
+  margin: 0 0  12px 0;
+  padding: 0;
+`
+
+const EventDate = styled.p`
+  margin: 0;
+  padding: 0;
+`
+
+const About = props => (
+  <AboutWrapper>
+    <Description>
+      <EventName>{props.eventName}</EventName>
+      <EventDate>{props.eventDate}</EventDate>
+    </Description>
+  </AboutWrapper>
+)
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <h1>All About This Site</h1>
-    <Link to="/about">Go to the "About" page.</Link>
+    <About
+      eventName="About 1"
+      eventDate="2020-01-02"
+      />
+    <About
+      eventName="About 2"
+      eventDate="2020-01-03"
+      />
   </Layout>
 )
 
